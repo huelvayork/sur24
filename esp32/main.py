9c81c090ac278eded549c0c61dd10e2e25ff7917
David Prieto <davidp@huelvayork.com>
2024-05-06 01:51:08 +0200

    handle flash lights

--- esp32/main.py
+++ esp32/main.py
@@ -1,18 +1,34 @@
 import time
 from machine import Pin, SoftI2C, UART, WDT
+import machine
 import ssd1306
 from a9g import A9G
 
-ID="MAZAKOTE"
-PHONE="606XXXXXX"
-
 import config
 
+LIGHTS_OFF=0
+LIGHTS_LEFT=-1
+LIGHTS_RIGHT=1
+LIGHTS_BOTH=3
+lights_state=LIGHTS_OFF
 
-#wdt = WDT(timeout=15000)
+LEFT_PIN=25
+RIGHT_PIN=26
+WARNING_PIN=33
+SMS_PIN=32
 
-button = Pin(33, mode=Pin.IN, pull=Pin.PULL_UP)
-last_buttonpress = 0
+left_button=Pin(LEFT_PIN, Pin.IN, Pin.PULL_UP)
+right_button=Pin(RIGHT_PIN, Pin.IN, Pin.PULL_UP)
+warning_button=Pin(WARNING_PIN, Pin.IN, Pin.PULL_UP)
+sms_button=Pin(SMS_PIN, Pin.IN, Pin.PULL_UP)
+
+left_relay=Pin(14, Pin.OUT)
+right_relay=Pin(13, Pin.OUT)
+left_relay.on()
+right_relay.on()
+
+lights_lasttime = 0
+last_smsbuttonpress = 0
 
 a9g = A9G(uart_id = 2)
 
@@ -37,17 +53,17 @@ def send_location_traccar():
     speed = a9g.gps.speed[0] # velocidad en nudos
     heading = a9g.gps.course
     
-    url = "http://cloud.huelvayork.com:5055/?id={}&lat={}&lon={}&speed={}&course={}".format(ID, lat, lon, speed, heading)
+    url = "{}/?id={}&lat={}&lon={}&speed={}&course={}".format(config.TRACCAR_URL,config.ID, lat, lon, speed, heading)
     a9g.http_get(url)
 
 
 def send_location_sms():
     global a9g
     if a9g.gps_fixed():
-        sms_text = "Vehiculo: {}\n".format(ID)
+        sms_text = "Vehiculo: {}\n".format(config.ID)
         sms_text = sms_text + "Posicion: {} {}\n".format(a9g.gps.latitude_string(), a9g.gps.longitude_string())
         sms_text = sms_text + "https://www.google.com/maps/search/?api=1&query={}{}+{}{}".format(a9g.gps.latitude[0], a9g.gps.latitude[1], a9g.gps.longitude[0], a9g.gps.longitude[1])
-        a9g.sms(PHONE, sms_text)
+        a9g.sms(config.PHONE, sms_text)
 
 
 def display_data():
@@ -62,20 +78,122 @@ def display_data():
     oled.show()
 
 
-def on_buttonpress(pin=None):
-    global last_buttonpress
-    now=time.time()
-    if last_buttonpress < now-1:
-        last_buttonpress = now
+def sms_buttonpress():
+    global last_smsbuttonpress
+    global must_send_sms
+    
+    now = time.time()
+    if now > last_smsbuttonpress + 5:
+        last_smsbuttonpress = now
         must_send_sms = True
+    
 
+on_buttonpress_lasttime=0
+def on_buttonpress(pin=None):
+    global lights_state
+    global on_buttonpress_lasttime
+
+    # Disable interrupts
+    irq_state = machine.disable_irq()
+
+    now = time.ticks_ms()
+    if (now < on_buttonpress_lasttime + 500):
+        print("{} paso".format(pin))
+        # Enable interrupts
+        machine.enable_irq(irq_state)
+        return
+    
+    on_buttonpress_lasttime = now
+    
+    if pin==sms_button:
+        lights_state = LIGHTS_BOTH
+        sms_buttonpress()
+
+    elif pin==left_button:
+        if lights_state in (LIGHTS_OFF, LIGHTS_LEFT):
+            lights_state = LIGHTS_LEFT
+        else:
+            lights_state = LIGHTS_OFF
+     
+    elif pin==right_button:
+        if lights_state in (LIGHTS_OFF, LIGHTS_RIGHT):
+            lights_state = LIGHTS_RIGHT
+        else:
+            lights_state = LIGHTS_OFF
+
+    elif pin==warning_button:
+        if lights_state == LIGHTS_BOTH:
+            lights_state = LIGHTS_OFF
+        else:
+            lights_state = LIGHTS_BOTH
+            
+    # Enable interrupts
+    machine.enable_irq(irq_state)        
+
+
+def lights_off():
+    left_relay.on()
+    right_relay.on()
+
+
+def lights_left():
+    global lights_lasttime
+    
+    now = time.ticks_ms()
+    if now < lights_lasttime + 400:
+        return
+
+    lights_lasttime = now
+
+    if left_relay.value() == 1:
+        left_relay.off()
+    else:
+        left_relay.on()
+
+
+def lights_right():
+    global lights_lasttime
+    
+    now = time.ticks_ms()
+    if now < lights_lasttime + 400:
+        return
+
+    lights_lasttime = now
+
+    if right_relay.value() == 1:
+        right_relay.off()
+    else:
+        right_relay.on()
+    
+
+def lights_both():
+    global lights_lasttime
+    
+    now = time.ticks_ms()
+    if now < lights_lasttime + 400:
+        return
+
+    lights_lasttime = now
+
+    if left_relay.value() == 1:
+        right_relay.off()
+        left_relay.off()
+    else:
+        right_relay.on()
+        left_relay.on()
+
+
+lights_off()
 
 a9g.conn_init()
 a9g.gps.local_offset = 2
 a9g.gps_init()
 a9g.gps_periodic_update(1)
 
-button.irq(on_buttonpress, trigger=Pin.IRQ_FALLING)
+sms_button.irq(on_buttonpress, trigger=Pin.IRQ_FALLING)
+left_button.irq(on_buttonpress, trigger=Pin.IRQ_FALLING)
+right_button.irq(on_buttonpress, trigger=Pin.IRQ_FALLING)
+warning_button.irq(on_buttonpress, trigger=Pin.IRQ_FALLING)
 
 display_time = time.time()
 traccar_time = time.time()
@@ -96,9 +214,16 @@ while True:
             send_location_traccar()
             traccar_time = now + 10
 
-
     if must_send_sms:
         must_send_sms = False
         send_location_sms()
 
-    
+    if lights_state == LIGHTS_OFF:
+        lights_off()
+    elif lights_state == LIGHTS_LEFT:
+        lights_left()
+    elif lights_state == LIGHTS_RIGHT:
+        lights_right()
+    elif lights_state == LIGHTS_BOTH:
+        lights_both()
+        
